3dbff50a0e5b300da863d6fe0ec754dc9e99b52f
J. Alexander <xandermoorman@gmail.com>
2017-05-30 20:50:30 -0500

    Update AnalyzePrefetch.scala

--- src/AnalyzePrefetch.scala
+++ src/AnalyzePrefetch.scala
@@ -12,7 +12,7 @@ import scala.io.Source
 	* http://www.hexacorn.com/blog/2012/06/13/prefetch-hash-calculator-a-hash-lookup-table-xpvistaw7w2k3w2k8/
 	*/
 
-object AnalyzePrefetch {
+object AnalyzePrefetch extends FileFun{
 
 	def main(Array[String]): Unit = {
 		val prefFileName = Source.fromFile("/Users/xan0/Documents/security/prefetch_hashes_lookup.txt").getLines.toArray
@@ -20,9 +20,13 @@ object AnalyzePrefetch {
 		val safePrefetchList = prefFileName.map(reg.findFirstIn(_).mkString)
 		val otherReg = """[A-Z0-9.]+""".r
 		val commonFiles = safePrefetchList.map(otherReg.findFirstIn(_).mkString).toSet.toArray
-
+		val dirArray = Array("/Users/username/Documents/prefetch-files")
+		val systemPrefetchFiles = getAllFiles(dirArray)
+		
 		/*
 				// NEXT we need to import all of the prefetches from directory and compare their names to Strings in the safePrefetchList.
+				// We also need to write a regex to extract the prefetch fileNames without the hash values appended to them to use as
+				// a comparison.
 				// If one of the comparisons with the filenames with hashes extracted matches, the program should alert the user.
 		*/
 	} // END main()
