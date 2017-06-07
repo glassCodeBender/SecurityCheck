3b065e1a8c2df12d41487eedcb305bc2d98a75b6
J. Alexander <xandermoorman@gmail.com>
2017-06-06 20:30:16 -0500

    Update AnalyzePrefetch.scala

--- src/AnalyzePrefetch.scala
+++ src/AnalyzePrefetch.scala
@@ -4,47 +4,66 @@ import scala.io.Source
 
 /**
 	* @Author: glassCodeBender
-	* @Date 5/31/2017
+	* @Date 6/6/2017
 	* @Version 1.0
+	*         
+	* Note: This program only works for Windows 7 and earlier. I'm not sure which 
+	* version of Windows Server this goes up to.
+	* 
+	* Other note: This class should almost exclusively use collections made up of 
+	* String objects.
 	*
-	* This program will look at a directory of prefetch files and determine inconsistencies.
+	* Program Purpose: This program looks at a directory full of prefetch files and locates 
+	* inconsistencies.
+	* 
 	* Note: The list of safe prefetch filenames was obtained here:
 	* http://www.hexacorn.com/blog/2012/06/13/prefetch-hash-calculator-a-hash-lookup-table-xpvistaw7w2k3w2k8/
-	*
 	*/
 
 object AnalyzePrefetch extends FileFun {
 
-	def main(Array[String]): Unit = {
+	/**
+		* MAIN METHOD
+		* @params prefetchDir: String - Stores the directory that contains the prefetchfiles.
+		*         lookupFile: String - Stores the full qualified domain name connected to the
+	  *         text file with the huge list of possible file names (from hexacorn.com).
+	  * @return Unit : prints to console. 
+		* */
 
-		val prefetchDirectory = "/Users/username/Documents/prefetchdir"   // stores prefetch directory location
+	def analyze(prefetchDir: String, // Stores the directory that contains the prefetch files.
+	            lookupFile: String) = { // Stores the file with the huge list of possible file names.
 
-		/** Generate an Array made up of legitimate prefetch filenames.*/
+		val prefetchDirectory = prefetchDir  // stores prefetch directory location
+
+		/** Generate an Array made up of legitimate prefetch filenames.
+			* An array is used because arrays are good for parallel processing. */
 		val reg = """[A-Z0-9]+.\w[-A-Z0-9]+.pf""".r
 		val safePrefetchArray = {
-			Source.fromFile("/Users/username/Documents/security/prefetch_hashes_lookup.txt")
+			Source.fromFile(lookupFile)
 				.getLines
 				.toArray
-				.map(reg.findFirstIn(_).mkString).par}
+				.map(reg.findFirstIn(_).mkString).par
+		}
 
-		/* Create an Array of made up of common system filenames. */
+		/* Create an Array made up of common system filenames. */
 		val otherReg = """[A-Z0-9.]+""".r
 		val commonFiles = safePrefetchArray.map(otherReg.findFirstIn(_).mkString).toSet.toArray.par
 
 		/*  import all of the prefetch files from a directory. */
-		val dirArray = Array(prefetchDirectory)
-		val systemPrefetchFiles = super.getAllFiles(dirArray).par
+		val systemPrefetchFiles = super.getAllFiles(Array(prefetchDirectory)).par
 
 		/* filter out the prefetch files that we have hash values for. */
-		val matchArray = systemPrefetchFiles.filter(x => commonFiles.exists(y => x.contains(y)))
+		val matchArray = systemPrefetchFiles.filter(x => commonFiles.exists(y => x.contains(y))).par
 
 		/* filter out the prefetch files that are not in the safePrefetchList */
-		val scaryFiles = matchArray.filter(x => safePrefetchArray.exists(y => x.contains(y)))
+		val scaryFiles = matchArray.filter(x => safePrefetchArray.exists(y => x.contains(y))).par
 
 		// Use the scaryFiles Array to determine which csv files need to be queried and appended to one another
 		// for further assessment.
 
+		println("Go to the prefetch directory and examine the following prefetch files:")
+		println()
 		scaryFiles.foreach(println)
 
-	} // END main()
+	} // END analyze()
 } // END AnalyzePrefetch
