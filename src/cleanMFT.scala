975267c648955eeaa7401af635fc374d6f5f4dc3
J. Alexander <xandermoorman@gmail.com>
2017-06-08 11:46:51 -0500

    Update cleanMFT.scala

--- src/cleanMFT.scala
+++ src/cleanMFT.scala
@@ -4,8 +4,8 @@ import java.io.IOException
 import java.sql.Timestamp
 
 import org.apache.hadoop.yarn.webapp.hamlet.HamletSpec.SELECT
-import org.apache.spark.sql.functions.unix_timestamp
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions._ // needed to do a lot of things (unix_timestamp)
 
 import scala.io.Source
 import scala.util.matching.Regex
@@ -28,7 +28,7 @@ object CleanMFT {
 	val filterIndex = false
 	val startIndex = None
 	val endIndex = None
-	val outputFile = ""
+	val outputFile = None
 	val indexBool = true
 	val suspicious = false
 	val startDate = None
@@ -57,8 +57,8 @@ object CleanMFT {
 
 		/* Create DataFrame and import MFT csv file. */
 		val sqlContext = new SQLContext(sc)
+
 		// WARNING!!!
-		// PIPE SEPARATED VALUE.
 		// No concatenation to create timestamps.
 		/* import csv file and convert it into a DataFrame */
 		val df = sqlContext.read.format("com.databricks.spark.csv")
@@ -69,18 +69,39 @@ object CleanMFT {
 
 		/* Filter DataFrame by index location */
 		if (startIndex != None || endIndex != None)
-			df = indexFilter(df, startIndex, endIndex )
-    /* Filter DataFrame to only include EXEs outside System32 or Program Files */
+			val indexDF = indexFilter(df, startIndex, endIndex )
+
+		/* Filter DataFrame to only include EXEs outside System32 or Program Files */
 		if(suspicious == true )
-			df = filterSuspicious(df)
+			val suspiciousDF = filterSuspicious(if (indexDF != None) filterSuspicious(indexDF) else filterSuspicious(df))
+
 		/* Filter DataFrame by list of Strings (Regex) */
-		if(regexFile != None )
-			df = filterByFilename( df )
+		if(regexFile != None ){
+			val regDF = {
+				if ( suspiciousDF != None ) filterByFilename ( suspiciousDF )
+				else if ( indexDF != None ) indexDF
+				else df
+			}
+		} // END if regexFile
+
+		/**
+			* Stores the current state of the DataFrame
+			* @return DataFrame
+			*/
+		val theDF: DataFrame = df match {
+			case regDF != None => regDF
+				case suspiciousDF != None => suspiciousDF
+				case indexDF != None => indexDF
+				case df != None => df
+		} // END theDF()
+
+		/* Take user input and convert it into a timestamp(s) */
 		if(startDate != None || endDate != None || startTime != None || endTime != None) {
-			/*Create Start and Stop Timestamps for filtering */
+
+				/*Create Start and Stop Timestamps for filtering */
 			val timeStamp = makeTimeStamp(startDate.mkString, endDate.mkString, startTime.mkString, endTime.mkString )
 
-			df = filterByDate(sqlContext, startStamp, endStamp)
+			val dateDF = filterByDate(sqlContext, theDF , startStamp, endStamp)
 		} // END if statement filter by date
 
 		df.saveAsSequenceFile("Users/Documents/MFT")
@@ -108,7 +129,19 @@ object CleanMFT {
 		 */
 
 	} // END run()
+	/********************************END OF THE DRIVER PROGRAM**********************************/
+  /*******************************************************************************************/
+	/*******************************************************************************************/
 
+	/**
+		* makeTimeStamp()
+		* Takes data with separate time and date columns and converts them into unix_timestamps
+		* @param startDate starting date
+		* @param endDate end date
+		* @param startTime start time
+		* @param endTime end time
+		* @return (unix_timestamp, unix_stamp) - Tuple with both timestamps
+		*/
 	def makeTimeStamp(startDate: String, // starting date
 	                  endDate: String, // end date
 	                  startTime: String, // start time
@@ -146,7 +179,7 @@ object CleanMFT {
 	  */
 	def filterByFilename(df: DataFrame): DataFrame = {
     val pattern = updateReg(regexFile).r // contains regex pattern
-		// this probably needs to be opposite notContains() SEE API.a
+		// this probably needs to be opposite notContains() SEE API.
 		val filteredDF = df.filter(_.contains(pattern))
 		filteredDF
 
@@ -180,6 +213,7 @@ object CleanMFT {
 		* @return DataFrame - Filtered to only include relevant virus names.
 		*/
 	def filterByDate(sqlContext: SQLContext,
+	                df: DataFrame,
 	                 sDate: String,
 	                 eDate: String
 	                ): DataFrame = {
@@ -201,4 +235,173 @@ object CleanMFT {
 		val regexString = regArray.fold("")((first, second) => first + "|" + second )
 		return regexString
 	} // END updateReg()
+
+
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*************************** THIS IS THE END OF THE PROGRAM ******************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*****************************************************************************************/
+	/*
+
+    """ This is the main method of the program. """
+    def run(self):
+        sdate, edate, stime, etime = self.__start_date, self.__end_date, self.__start_time, self.__end_time
+        output_file = self.__output_file
+        suspicious = self.__suspicious
+        mft_csv = self.__file
+        reg_file = self.__reg_file
+        index_bool = self.__index_bool
+
+        sindex, eindex = [x.strip() for x in self.__filter_index.split(',')]
+        if sindex.contains(',') or eindex.contains(','):
+            sindex.replace(',', '')
+            eindex.replace(',', '')
+        if not sindex.isdigit and eindex.isdigit:
+            raise ValueError("ERROR: The index value you entered to filter the table by was improperly formatted. \n"
+                             "Please try to run the program again with different values.")
+        df = pd.DataFrame()
+        df = df.from_csv(mft_csv, sep='|', parse_dates=[[0, 1]])
+        # df = df.from_csv("MftDump_2015-10-29_01-27-48.csv", sep='|')
+        # df_attack_date = df[df.index == '2013-12-03'] # Creates an extra df for the sake of reference
+        if index_bool:
+            df.reset_index(level=0, inplace=True)
+            if sindex and eindex:
+                df = df[sindex : eindex]
+        if reg_file:
+            df = self.filter_by_filename(df)
+        if suspicious:
+            df = self.filter_suspicious(df)
+        if sdate or edate or stime or etime:
+            df = self.filter_by_dates(df)
+        df.to_csv(output_file, index=True)
+
+    """
+    Read a file line by line and return a list with items in each line.
+    @Param A Filename
+    @Return A list
+    """
+    def read_file(self, file):
+        list = []
+        with open(file) as f:
+            for line in f:
+                list.append(line)
+        return list
+
+    """
+    Method to filter a list of words and concatenate them into a regex
+    @Param List of words provided by user to alternative file.
+    @Return String that will be concatenated to a regex.
+    """
+    def update_reg(self, list):
+        s = '|'
+        new_reg = s.join(list)
+        return new_reg
+
+    """
+    Filters a MFT csv file that was converted into a DataFrame to only include relevant extensions.
+    @Param: DataFrame
+    @Return: DataFrame - Filtered to only include relevant file extensions.
+    """
+    def filter_by_filename(self, df):
+        reg_file = self.__reg_file
+        reg_list = self.read_file(reg_file)
+        user_reg = self.update_reg(reg_list)
+
+        if user_reg is not None:
+            pattern = r'' + user_reg
+        else:
+            pattern = r'.exe|.dll|.rar|.sys|.jar'
+
+        regex1 = re.compile(pattern, flags=re.IGNORECASE)
+        df['mask'] = df[['Filename', 'Desc']].apply(lambda x: x.str.contains(regex1, regex=True)).any(axis=1)
+        filt_df = df[df['mask'] == True]
+
+        pattern2 = r'Create$|Entry$'
+        regex2 = re.compile(pattern2, flags=re.IGNORECASE)
+        filt_df['mask2'] = filt_df[['Type']].apply(lambda x: x.str.contains(regex2, regex=True)).any(axis=1)
+        filtered_df = filt_df[filt_df['mask2'] == True]
+        filtered_df.drop(['mask', 'mask2'], axis=1, inplace=True)
+
+        return filtered_df
+
+    """
+    Filters a MFT so that only the executables that were run outside Program Files are
+    included in the table.
+    @Param: DataFrame
+    @Return: DataFrame - Filtered to only include relevant file extensions.
+    """
+    def filter_suspicious(self, df):
+        pattern = r'^.+(Program\sFiles|System32).+[.exe]$'
+        regex1 = re.compile(pattern)
+        df['mask'] = df[['Filename', 'Desc']].apply(lambda x: x.str.contains(regex1, regex=True)).any(axis=1)
+        filt_df = df[df['mask'] == False]
+
+        pattern2 = r'.exe$'
+        regex2 = re.compile(pattern2)
+        filt_df['mask2'] = filt_df[['Filename', 'Desc']].apply(lambda x: x.str.contains(regex2, regex=True)).any(axis=1)
+        filtered_df = filt_df[filt_df['mask2'] == True]
+        filtered_df.drop(['mask', 'mask2'], axis=1, inplace=True)
+        return filtered_df
+
+    """
+    Filters a MFT csv file that was converted into a Dataframe to only include the
+    occurrences of certain dates and/or times.
+    @Param: DataFrame
+    @Return: DataFrame - Filtered to only include relevant virus names.
+    """
+    def filter_by_dates(self, df):
+
+        sdate = self.__start_date
+        edate = self.__end_date
+        stime = self.__start_time
+        etime = self.__end_time
+
+        if edate and sdate and etime and stime:
+            s_stamp = pd.Timestamp(sdate + ' ' + stime)
+            e_stamp = pd.Timestamp(edate + ' ' + etime)
+            filtered_df = df[s_stamp:e_stamp]
+        elif sdate and edate and etime and not stime:
+            s_stamp = pd.Timestamp(sdate)
+            e_stamp = pd.Timestamp(edate + ' ' + etime)
+            filtered_df = df[s_stamp:e_stamp]
+        elif sdate and edate and stime:
+            s_stamp = pd.Timestamp(sdate + ' ' + stime)
+            e_stamp = pd.Timestamp(edate)
+            filtered_df = df[s_stamp:e_stamp]
+        elif sdate and stime:
+            s_stamp = pd.Timestamp(sdate + ' ' + stime)
+            filtered_df = df[s_stamp:]
+        elif edate and etime:
+            e_stamp = pd.Timestamp(edate + ' ' + etime)
+            filtered_df = df[:e_stamp]
+        elif sdate:
+            s_stamp = pd.Timestamp(sdate)
+            filtered_df = df[s_stamp:]
+        elif edate:
+            e_stamp = pd.Timestamp(edate)
+            filtered_df = df[:e_stamp]
+        else:
+            raise ValueError("You entered an invalid date to filter the table by or you did not include a date\n"
+                             "to filter by. Please try again."
+                             "\n\tExample Usage: $ python cleanMFT.py -f MFT.csv -r regex.csv -s 2015-06-08 -e 2015-06-30 -t 06:30:00 -u 06:31:20")
+        return filtered_df
+ */
 } // END CleanMFT.scala
