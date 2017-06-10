cd724efd7777f0dc4a237e68e8ce80af061252ac
J. Alexander <xandermoorman@gmail.com>
2017-06-10 11:07:52 -0500

    Create CleanMFT.scala

--- src/CleanMFT.scala
+++ src/CleanMFT.scala
@@ -21,150 +21,149 @@ import scala.util.matching.Regex
 	*          environments with Apache Spark.
 	*/
 
-class CleanMFT {
-	val sqlContext = new SQLContext(sc) // Create mothership.
+class CleanMFT(val sqlContext: SQLContext,
+               val iFile: String,
+               val regFile: String,
+               val oFile: String ){
 
-	/** main()
-		* Actual main method.
-		* @return Unit
-		*/
-	def main(args:Array[String]): Unit = {
-		val importFile: String = "/Users/mrsqueaksharem/Downloads/supers.csv"
-		val regexFile: String = "/Users/mrsqueaksharem/Downloads/files-to-filter.txt"
-		val outputFile = ""
+	/* Class will accept a SQLContext through it's constructor */
+	val spark = sqlContext
 
-		run(importFile, regexFile, outputFile)
-	} // END main()
+	/* Stores all the file locations the program uses. */
+	val importFile: String = iFile // The fully qualified file name for the MFT Dump CSV.
+	val regexFile: String = regFile // A text file with different items on each line to use for filter.
+	val outputFile = oFile // What should the csv file that is generated be named?
 
 	/**
 		* run()
 		* This method does all the work.
+		*
 		* @param importFile String  File that contains the MFT table as a CSV.
-		* @param regexFile String   Text file we will use to get values to filter with.
+		* @param regexFile  String   Text file we will use to get values to filter with.
 		* @param outputFile String  Name of the csv file we want to create.
 		* @return Unit
-		* */
-	def run(importFile: String, // File that contains the MFT table as a CSV.
-	        regexFile: String,  // Text file we will use to get values to filter with.
-	        outputFile: String  // Name of the csv file we want to create.
-	       ): Unit = {
+		**/
+	def run ( importFile: String, // File that contains the MFT table as a CSV.
+	          regexFile: String, // Text file we will use to get values to filter with.
+	          outputFile: String // Name of the csv file we want to create.
+	        ): Unit = {
 
 		/* String Filenames of different user input values. */
-		val tableFile = importFile     // stores the csv file location
+		val tableFile = importFile // stores the csv file location
 		val filterForRFile = regexFile // stores the user created list file location.
-		val destFileName = outputFile  // stores the name the user wants to use for the destination file.
+		val destFileName = outputFile // stores the name the user wants to use for the destination file.
 
 		/* Booleans to determine how the table should be filtered. */
-		val filterIndex = false        // Filter the index for some reason?
-		val startIndex = None          // Value of the field that user wants to start filtering from.
-		val endIndex = None            // Value of the field that user wants to stop filtering at.
-		val indexBool = true           // Does the user want toadd
-		val suspicious = false         // Filter any exe that ran outside of System32 or Program Files.
+		val filterIndex = false // Filter the index for some reason?
+		val startIndex = None // Value of the field that user wants to start filtering from.
+		val endIndex = None // Value of the field that user wants to stop filtering at.
+		val indexBool = true // Does the user want toadd
+		val suspicious = false // Filter any exe that ran outside of System32 or Program Files.
 
 		/* These are used to create unix_timestamp objects. */
-		val startDate = None           // Filter from this start date.
-		val endDate = None             // Filter until this end date.
-		val startTime = None           // Filter from this start time
-		val endTime = None             // Filter until this end time.
-
+		val startDate = None // Filter from this start date.
+		val endDate = None // Filter until this end date.
+		val startTime = None // Filter from this start time
+		val endTime = None // Filter until this end time.
 
 		// WARNING!!!
 		// No concatenation to create timestamps.
 		/* import csv file and convert it into a DataFrame */
-		val df = sqlContext.read.format("com.databricks.spark.csv")
-			.option("delimiter", "|" )
-			.option("header" = true)
-			.option("inferSchema", true)
-			.load(importFile).persist()
+		val df = spark.read.format ( "com.databricks.spark.csv" )
+			.option ( "delimiter", "|" )
+			.option ( "header" = true )
+			.option ( "inferSchema", true )
+			.load ( importFile ).persist ( )
 
 		/* Filter DataFrame by index location */
-		if (startIndex != None || endIndex != None)
-			val indexDF = indexFilter(df, startIndex, endIndex )
+		if ( startIndex != None || endIndex != None )
+		val indexDF = indexFilter ( df, startIndex, endIndex )
 
 		/* Filter DataFrame to only include EXEs outside System32 or Program Files */
-		if(suspicious == true )
-			val suspiciousDF = filterSuspicious(
-				if (indexDF != None) filterSuspicious(indexDF)
-				else filterSuspicious(df))
+		if ( suspicious == true )
+		val suspiciousDF = filterSuspicious (
+			if ( indexDF != None ) filterSuspicious ( indexDF )
+			else filterSuspicious ( df ) )
 
 		/* Filter DataFrame by list of Strings (Regex) */
-		if(!regexFile.isEmpty ){
+		if ( !regexFile.isEmpty ) {
 			val regDF = {
 				if ( suspiciousDF != None ) filterByFilename ( suspiciousDF )
-				else if ( indexDF != None ) => indexDF
-				else => df
+				else if ( indexDF != None ) indexDF
+				else df
 			}
 		} // END if regexFile
 
 		/**
 			* Stores the current state of the DataFrame
+			*
 			* @return DataFrame
 			*/
 		val theDF: DataFrame = {
 			if ( regDF != None ) regDF
-				else if (suspiciousDF != None) suspiciousDF
-				else if (indexDF != None) indexDF
-				else df
+			else if ( suspiciousDF != None ) suspiciousDF
+			else if ( indexDF != None ) indexDF
+			else df
 		} // END theDF
 
 		/* Take user input and convert it into a timestamp(s) */
-		if(startDate != None || endDate != None || startTime != None || endTime != None) {
+		if ( startDate != None || endDate != None || startTime != None || endTime != None ) {
 
 			/*Create Start and Stop Timestamps for filtering */
-			val timeStamp = makeTimeStamp(startDate.mkString, endDate.mkString, startTime.mkString, endTime.mkString )
+			val timeStamp = makeTimeStamp ( startDate.mkString, endDate.mkString, startTime.mkString, endTime.mkString )
 
-			val dateDF = filterByDate( theDF , timeStamp._1, timeStamp._2)
+			val dateDF = filterByDate ( theDF, timeStamp._1, timeStamp._2 )
 		} // END if statement filter by date
 
 		/* Save the processed Data to a compressed file. */
-    if (dateDF != None) dateDF.saveAsSequenceFile("Users/lupefiascoisthebestrapper/Documents/MFT")
-		else theDF.saveAsSequenceFile("Users/lupefiascoisthebestrapper/Documents/MFT")
-
-		/* Filter DataFrame by Date*/
-
+		if ( dateDF != None ) dateDF.saveAsSequenceFile ( "Users/lupefiascoisthebestrapper/Documents/MFT" )
+		else theDF.saveAsSequenceFile ( "Users/lupefiascoisthebestrapper/Documents/MFT" )
 
 		// if option to filter by index is true where do we get the index locations?
 		// probably a method.
 
 	} // END run()
-	/********************************END OF THE DRIVER PROGRAM**********************************/
-  /*******************************************************************************************/
-	/*******************************************************************************************/
+	/** ******************************END OF THE DRIVER PROGRAM **********************************/
+	/** *****************************************************************************************/
+	/** *****************************************************************************************/
 
 	/**
 		* makeTimeStamp()
 		* Takes data with separate time and date columns and converts them into unix_timestamps
+		*
 		* @param startDate starting date
-		* @param endDate end date
+		* @param endDate   end date
 		* @param startTime start time
-		* @param endTime end time
+		* @param endTime   end time
 		* @return (unix_timestamp, unix_stamp) - Tuple with both timestamps
 		*/
-	def makeTimeStamp(startDate: String, // starting date
-	                  endDate: String, // end date
-	                  startTime: String, // start time
-	                  endTime: String  // end time
-	                 ) = {
+	def makeTimeStamp ( startDate: String, // starting date
+	                    endDate: String, // end date
+	                    startTime: String, // start time
+	                    endTime: String // end time
+	                  ) = {
 		val start = ( startDate.mkString + " " + startTime.mkString )
 		val end = ( endDate.mkString + " " + endTime.mkString )
 		/*Create Start and Stop Timestamps for filtering */
-		val startStamp: unix_timestamp(start)
-		val endStamp = unix_timestamp(end)
+		val startStamp: unix_timestamp = start
+		val endStamp: unix_timestamp = end
 		(startStamp, endStamp) // returns tuple with start and end timestamps
+
 	} // END makeTimeStamp()
 
 	/**
 		* indexFilter()
 		* Filters a DataFrame based on start and ending index locations.
+		*
 		* @param df     DataFrame
 		* @param sIndex Start Index
 		* @param eIndex End Index
 		* @return DataFrame
 		*/
-	def indexFilter( df: DataFrame, // Accepts a DataFrame.
-	                sIndex: Int,   // Integer value that represents starting index.
-	                eIndex: Int    // Integer value that represents the end index.
-	               ): DataFrame = {
+	def indexFilter ( df: DataFrame, // Accepts a DataFrame.
+	                  sIndex: Int, // Integer value that represents starting index.
+	                  eIndex: Int // Integer value that represents the end index.
+	                ): DataFrame = {
 
 		// DO SQL
 
@@ -172,15 +171,16 @@ class CleanMFT {
 
 	/**
 		* filterByFilename()
-    * Filters a MFT csv file that was converted into a DataFrame to only include relevant extensions.
-    * @param df DataFrame
-    * @return DataFrame - Filter df to only include relevant file extensions.
-    * @throws IOException explains why certain common behaviors occurs
-	  */
-	def filterByFilename(df: DataFrame): DataFrame = {
-    val pattern = updateReg(regexFile).r // contains regex pattern
+		* Filters a MFT csv file that was converted into a DataFrame to only include relevant extensions.
+		*
+		* @param df DataFrame
+		* @return DataFrame - Filter df to only include relevant file extensions.
+		* @throws IOException explains why certain common behaviors occurs
+		*/
+	def filterByFilename ( df: DataFrame ): DataFrame = {
+		val pattern = updateReg ( regexFile ).r // contains regex pattern
 		// this probably needs to be opposite notContains() SEE API.
-		val filteredDF = df.filter(_.contains(pattern))
+		val filteredDF = df.filter ( _.contains ( pattern ) )
 		filteredDF
 
 	} // END filterByFilename()
@@ -189,16 +189,18 @@ class CleanMFT {
 		* filterSuspicious()
 		* Filters a MFT so that only the executables that were run outside Program Files are
 		* included in the table.
+		*
 		* @param df DataFrame
 		* @return DataFrame - Filtered to only include relevant file extensions.
-    */
-	def filterSuspicious(df: DataFrame): DataFrame = {
+		*/
+	def filterSuspicious ( df: DataFrame ): DataFrame = {
 		// matches all Strings that ran in Program Files or System32
-		val regexSys32 = """^.+(Program\sFiles|System32).+[.exe]$""".r
-		val regexExe = """.exe$""".r  // matches all Strings that end with .exe
+		val regexSys32 =
+			"""^.+(Program\sFiles|System32).+[.exe]$""".r
+		val regexExe = """.exe$""".r // matches all Strings that end with .exe
 
-		val filterDF1 = df.filter(_.contains(regexSys32))
-		val filteredDF = filteredDF1.filter(_.contains(regexExe))
+		val filterDF1 = df.filter ( _.contains ( regexSys32 ) )
+		val filteredDF = filteredDF1.filter ( _.contains ( regexExe ) )
 
 		filteredDF
 	} // END filterSuspicious()
@@ -207,36 +209,38 @@ class CleanMFT {
 		* filterByDate()
 		* Filters a MFT csv file that was converted into a Dataframe to only include the
 		* occurrences of certain dates and/or times.
-		* @param df DataFrame
+		*
+		* @param df    DataFrame
 		* @param sDate String
 		* @param eDate String
 		* @return DataFrame - Filtered to only include relevant virus names.
 		*/
-	def filterByDate( df: DataFrame,
-	                  sDate: unix_timestamp,
-	                  eDate: unix_timestamp
-	                ): DataFrame = {
+	def filterByDate ( df: DataFrame,
+	                   sDate: unix_timestamp,
+	                   eDate: unix_timestamp
+	                 ): DataFrame = {
 
-		val dateDF = sqlContext.sql( SELECT *
-			                           WHERE $Date_Time >= sDate AND $Date_Time =< $eDate )
+		val dateDF = spark.sql ( SELECT *
+			WHERE $Date_Time >= sDate AND $Date_Time =< $eDate )
 		dateDF
 	} // END filterByDate()
 
 	/**
 		* updateReg()
 		* Filters a list of words and concatenate them into a regex.
+		*
 		* @param fileName String made up of words provided by users to filter table with.
 		* @forExample Concatenates each line of a text file into a regular expression.
 		* @return Regex
 		*/
-	def updateReg(fileName: String): String = {
+	def updateReg ( fileName: String ): String = {
 		// import file - this can also be imported directly into a DataFrame
-		val regArray = Source.fromFile(fileName).getLines.toArray.map(_.trim).par
-    // concatenate each member of the array to make String
-		val regexString = regArray.fold("")((first, second) => first + "|" + second )
+		val regArray = Source.fromFile ( fileName ).getLines.toArray.map ( _.trim ).par
+		// concatenate each member of the array to make String
+		val regexString = regArray.fold ( "" )( ( first, second ) => first + "|" + second )
 		return regexString
 	} // END updateReg()
-
+} // END CleanMFT.scala
 
 	/*****************************************************************************************/
 	/*****************************************************************************************/
@@ -361,48 +365,3 @@ class CleanMFT {
         filtered_df = filt_df[filt_df['mask2'] == True]
         filtered_df.drop(['mask', 'mask2'], axis=1, inplace=True)
         return filtered_df
-
-    """
-    Filters a MFT csv file that was converted into a Dataframe to only include the
-    occurrences of certain dates and/or times.
-    @Param: DataFrame
-    @Return: DataFrame - Filtered to only include relevant virus names.
-    """
-    def filter_by_dates(self, df):
-
-        sdate = self.__start_date
-        edate = self.__end_date
-        stime = self.__start_time
-        etime = self.__end_time
-
-        if edate and sdate and etime and stime:
-            s_stamp = pd.Timestamp(sdate + ' ' + stime)
-            e_stamp = pd.Timestamp(edate + ' ' + etime)
-            filtered_df = df[s_stamp:e_stamp]
-        elif sdate and edate and etime and not stime:
-            s_stamp = pd.Timestamp(sdate)
-            e_stamp = pd.Timestamp(edate + ' ' + etime)
-            filtered_df = df[s_stamp:e_stamp]
-        elif sdate and edate and stime:
-            s_stamp = pd.Timestamp(sdate + ' ' + stime)
-            e_stamp = pd.Timestamp(edate)
-            filtered_df = df[s_stamp:e_stamp]
-        elif sdate and stime:
-            s_stamp = pd.Timestamp(sdate + ' ' + stime)
-            filtered_df = df[s_stamp:]
-        elif edate and etime:
-            e_stamp = pd.Timestamp(edate + ' ' + etime)
-            filtered_df = df[:e_stamp]
-        elif sdate:
-            s_stamp = pd.Timestamp(sdate)
-            filtered_df = df[s_stamp:]
-        elif edate:
-            e_stamp = pd.Timestamp(edate)
-            filtered_df = df[:e_stamp]
-        else:
-            raise ValueError("You entered an invalid date to filter the table by or you did not include a date\n"
-                             "to filter by. Please try again."
-                             "\n\tExample Usage: $ python cleanMFT.py -f MFT.csv -r regex.csv -s 2015-06-08 -e 2015-06-30 -t 06:30:00 -u 06:31:20")
-        return filtered_df
- */
-} // END CleanMFT.scala
